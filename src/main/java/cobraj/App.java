7669335ac592ceda327337299e05da46e0ed0f2d
Tom Monk <tom@jtmonk.com>
2016-07-16 22:45:21 -0700

    Add Connecting and Authenticating example code

--- src/main/java/cobraj/App.java
+++ src/main/java/cobraj/App.java
@@ -15,12 +15,25 @@ public class App {
 		PythonInterpreter interpreter = new PythonInterpreter();
 		interpreter.exec("import sys");
 		interpreter.exec("print(sys.path)");
-		interpreter.exec("import cobra.mit");
-		interpreter.exec("print(dir(cobra.mit))");
+		interpreter.exec("import cobra.mit.naming");
+		interpreter.exec("print(dir(cobra.mit.naming))");
+
+		// This example comes from the Cisco APIC Python API Documentation
+		// (Release 0.1), section 6.2: Connecting and Authenticating
+//		LoginSessionType loginSession = LoginSessionFactory.create(
+//				new PyString("https://192.168.10.80"),
+//				new PyString("admin"),
+//				new PyString("mypassword"));
+//		MoDirectoryType moDir = MoDirectoryFactory.create(loginSession);
+//		moDir.login();
+//		// # Use the connected moDir queries and configuration...
+//		moDir.logout();
 
 		DnFactory dnFactory = new DnFactory();
 		DnType dn = dnFactory.create(Arrays.<PyObject> asList(
-				new PyString("uni"), new PyString("userext"), new PyString("user-john")));
+				new PyString("uni"),
+				new PyString("userext"),
+				new PyString("user-john")));
 		System.out.println(dn.__str__());
 
 	}
